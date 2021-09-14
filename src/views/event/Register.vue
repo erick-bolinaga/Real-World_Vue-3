34d834ce42383ff16b5e9572891baf3ba6941606
erick_bolinaga <habanero01@gmail.com>
2021-09-15 00:21:32 +0300

    Lesson 4 - 7

---
+++ src/views/event/Register.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>Register for the event here</p>
+    <button @click="register">Register me</button>
+</template>
+
+<script>
+export default {
+    props: ['event'],
+    methods: {
+        register() {
+            // Call to API
+            // If registered then redirect to event details
+
+            this.$router.push({
+                name: "EventDetails",
+                // params: { id: this.event.id } ... Since is a nested route and id is required by the parent route, we don't need to pass it
+            });
+            // this.$router.replace ... The same like push but without creating a history entry
+            // this.$router.go(1)  or -1 .. move forward or backward in the history
+        }
+    }
+}
+</script>
